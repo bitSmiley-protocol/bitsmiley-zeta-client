0dbe87c65db5220bbcddad6eeec2b7cfe88d6568
dev <dev@bitsmiley.io>
2024-11-19 15:41:48 +0800

    stringfy

--- src/client.ts
+++ src/client.ts
@@ -231,7 +231,7 @@ class RevealTxnBuilder {
             case testnet:
                 return DEFAULT_CONFIG.tss.testnet;
             default:
-                throw Error(`not supporting ${this.network}`);
+                throw Error(`not supporting ${JSON.stringify(this.network)}`);
         }
     }
 }
